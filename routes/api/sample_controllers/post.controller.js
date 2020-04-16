a81a969099309ce801c0f269108ec232690c7e45
Mohammed Magdy Ismael <m.magdy.isl@gmail.com>
2020-04-16 12:09:00 +0200

    One layer crawler

--- routes/api/sample_controllers/post.controller.js
+++ routes/api/sample_controllers/post.controller.js
@@ -1,9 +1,90 @@
-const {validationResult } = require('express-validator');
+var request = require("request");
+var domParser = require("dom-parser")
 
-exports.postSample = async (req, res, next) =>{
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(400).json({msg:errors.array()})
+
+function paragraphToWordBag (body, q) {
+  var links = [];
+  var doc = new domParser().parseFromString(body, "text/html");
+  var bodyContent = doc.getElementById("bodyContent").innerHTML;
+  // Get All Links
+  var currentPageLinks = doc.getElementsByTagName("a")
+  for(var i =0; i <= currentPageLinks.length;i++){
+    if(currentPageLinks[i] &&
+      currentPageLinks[i].getAttribute("href")&&
+      !currentPageLinks[i].getAttribute("href").includes(".jpg")&&
+      !currentPageLinks[i].getAttribute("href").includes(".png")&&
+      !currentPageLinks[i].getAttribute("href").includes(".gif")&&
+      !currentPageLinks[i].getAttribute("href").includes(".jpeg")&&
+      !currentPageLinks[i].getAttribute("href").includes(":")&&
+      !currentPageLinks[i].getAttribute("href").includes("#",0)){
+        if(currentPageLinks[i].getAttribute("href").includes("/",0)){
+          links.push(q.protocol+"//"+q.hostname+currentPageLinks[i].getAttribute("href"));
+        }else{
+          links.push(currentPageLinks[i].getAttribute("href"));
+        }
+    }
+  }
+  
+  // Extract pure text paragraph
+  var target = bodyContent.replace(/<[^>]*>/gi, ""); // Remove html tags
+  target = target.replace(/\r?\n|\r|\t/gi, "");
+  // Create word bag
+  target = target.toLowerCase();
+  var wordArray = target.split(" ");
+  const unique = [...new Set(wordArray)]
+  const excludedWords = [
+    'the', 'a', 'an', 'and', 
+    'he', 'she', 'it', 'they', 'you',
+    'them', 'their', 'his', 'her', 'your',
+    'what', 'where', 'who', 'when', 'that',
+    'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+    'eight', 'nine', 'ten', 'hundred', 'thousand', 'million', 'from',
+    'to', 'few', 'less', 'january', 'after', 'in', 'into','inside',
+    'about', 'for', 'main', 'job', 'many']
+  const filterExcludedWords = unique.filter(word => {
+    if(!excludedWords.includes(word) &&
+     !(/\W/.test(word)) &&
+     !(/\d/.test(word)) &&
+     word.length > 2
+     ) return word;
+  })
+
+  var obj = {}
+  filterExcludedWords.forEach(element => {
+    var count = 0;
+    for(var i = 0; i<= wordArray.length; i++){
+      if(element === wordArray[i]){
+        count = count + 1; 
+      }
+    }
+    obj[element]=count;
+  })
+  return obj
+}
+
+exports.getWiki =  async (req, res, next) => {
+    try {
+      const promise1 = new Promise(function(resolve, reject) {
+        var url = require('url');
+        var adr = req.body.wikiURL;
+        var q = url.parse(adr, true);
+        request({uri:adr,},(e,rep,body)=>{
+          resolve(paragraphToWordBag(body, q));
+        });
+      });
+      let y = undefined;
+      let prom1 = promise1.then(function(value) {
+        return value;
+      });
+      y = await Promise.all([prom1]).then((v)=>{return v})
+      var obj = {
+        status: 200,
+        data: y[0],
+      }
+      res.status(200).send(obj)
+    } catch (err) {
+      console.log(err)
+      res.status(500).send('Server Error');
     }
     next()
-}
+  }
--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const Joi = require('@hapi/joi');
-const assert = require('assert');
+const bodyParser = require('body-parser'); 
 
-const connectDB = require('./config/database/mongoAtlas') 
+/* const connectDB = require('./config/database/mongoAtlas')  
+const {bugsnagmiddleware, bugsnagClient} = require('./config/bugsnag') */
 const swaggerSetup = require('./config/swagger')
-const {bugsnagmiddleware, bugsnagClient} = require('./config/bugsnag')
-
 const app = express();
-connectDB();
+/* connectDB(); */
 
 app.use(express.json({extended : false}))
-app.use(bugsnagmiddleware.requestHandler)
-app.use(bugsnagmiddleware.errorHandler)
+/* app.use(bugsnagmiddleware.requestHandler)
+app.use(bugsnagmiddleware.errorHandler) */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //bugsnagClient.notify(new Error('Test error'))
 app.use('/api/sample',require('./routes/api/sample'))
-
-app.get('/',(req,res)=>{
-    try{
-        bugsnagClient.notify("s")
-        return res.send("Social Network APIs")
-    }
-    catch(e){
-        bugsnagClient.notify(e)
-    }  
-})
 
 const PORT = process.env.PORT || 5000;
 swaggerSetup(PORT, app);
